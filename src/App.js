c268e66443e95abfc91ffa221f53faceefa350e2
Gnaneswar Sai Kumar Konduru <gnanee57@gmail.com>
2021-09-04 21:55:41 +0530

    redirect successful

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import { Switch, Route, Redirect, BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+          <div className="App">
+              <Switch>
+                  <Route
+                      path="/home"
+                      component={() => {
+                          window.location.href = 'https://nilgiri-house.netlify.app';
+                          return null;
+                      }} />
+                  <Redirect to="/home"  />
+              </Switch>
+          </div>
+      </BrowserRouter>
   );
 }
 
